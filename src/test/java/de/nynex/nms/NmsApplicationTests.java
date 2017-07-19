d8284db60ead6fe0685a6ea6b3b3d78fb23ed70f
Nynex <Nynex@192.168.0.189>
2017-07-19 07:40:30 +0200

    first commit

---
+++ src/test/java/de/nynex/nms/NmsApplicationTests.java
@@ -0,0 +1,16 @@
+package de.nynex.nms;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NmsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
